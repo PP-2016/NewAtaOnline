33098d1b9574b82bface59adae93344b5bf930b4
Gustavo Sabino <gustavo.sabino@hotmail.com>
2016-04-05 13:00:36 -0300

    Models for student, professor and notebook

--- AtaOnline/Ata/models.py
+++ AtaOnline/Ata/models.py
@@ -5,12 +5,35 @@ from django.db import models
 # Create your models here.
 
 
-class User(models.Model):
-    """User's docstring."""
+class Person(models.Model):
+    """Models for AtaOnline's user."""
 
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     number_id = models.IntegerField()
     email = models.EmailField(max_length=200)
-    name = models.CharField()
+    name = models.CharField(max_length=50)
     admin = models.BooleanField()
+
+
+class Student(models.Person, models.Model):
+    """Student's docstring."""
+
+
+class Professor(models.Person, models.Model):
+    """Professor's docstring."""
+
+    formation = models.CharField(max_length=200)
+
+
+# class Avaliation(models.Model):
+#     """Avaliation from a Professor for a Student's Notebook."""
+#     ata = Fo
+
+class Notebook(models.Model):
+    """Student's Notebook for class of Experimental Fisics."""
+
+    title = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True)
+    content = models.CharField(max_length=150)
+    avaliation = models.OneToOneField()
