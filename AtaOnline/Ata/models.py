b7803b458e4e3bf6a20db2fc3aeca6096e73cd21
Gustavo Sabino <gustavo.sabino@hotmail.com>
2016-04-05 12:00:30 -0300

    User's model

--- AtaOnline/Ata/models.py
+++ AtaOnline/Ata/models.py
@@ -1,3 +1,16 @@
+"""Models for Ata."""
+
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    """User's docstring."""
+
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    number_id = models.IntegerField()
+    email = models.EmailField(max_length=200)
+    name = models.CharField()
+    admin = models.BooleanField()
