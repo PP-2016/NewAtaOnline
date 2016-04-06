0c1ca3cafd943322869cf02892256cf7aa459a24
Macário Soares <macario.junior@gmail.com>
2016-04-06 11:57:29 -0300

    Updating urls to create a new user

--- AtaOnline/Ata/urls.py
+++ AtaOnline/Ata/urls.py
@@ -1,10 +1,11 @@
 """Ata urls."""
 
 from django.conf.urls import url
-from .views import Login, Index, SingUp
+from .views import Login, Index, SignUp
 urlpatterns = [
     url(r'^$', Index.as_view(), name="index"),
     url(r'^login/$', Login.as_view(), name="login"),
-    url(r'^signup/$', SingUp.as_view(), name="create"),
+    url(r'^signup/$', SignUp.as_view(), name="create"),
+    url(r'^createuser/$', SignUp.as_view(), name="post"),
 
 ]
