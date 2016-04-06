87e0ec00b3c437d1ad98293dcde3c78ca72d84e5
Macário Soares <macario.junior@gmail.com>
2016-04-06 11:26:11 -0300

    Adjusting login and create user views

--- AtaOnline/Ata/migrations/0001_initial.py
+++ AtaOnline/Ata/migrations/0001_initial.py
@@ -2,60 +2,65 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
+import django.contrib.auth.models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('auth', '0006_require_contenttypes_0002'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Avaliation',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
             ],
         ),
         migrations.CreateModel(
             name='Notebook',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
                 ('title', models.CharField(max_length=50)),
                 ('date', models.DateField(auto_now=True)),
-                ('content', models.CharField(max_length=150)),
+                ('content', models.CharField(max_length=250)),
                 ('grade', models.ForeignKey(to='Ata.Avaliation')),
             ],
         ),
         migrations.CreateModel(
             name='Professor',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
-                ('username', models.CharField(max_length=50)),
-                ('password', models.CharField(max_length=50)),
+                ('user_ptr', models.OneToOneField(primary_key=True, serialize=False, auto_created=True, parent_link=True, to=settings.AUTH_USER_MODEL)),
                 ('number_id', models.IntegerField()),
-                ('email', models.EmailField(max_length=200)),
-                ('name', models.CharField(max_length=50)),
-                ('admin', models.BooleanField()),
                 ('formation', models.CharField(max_length=200)),
             ],
             options={
                 'abstract': False,
+                'verbose_name': 'user',
+                'verbose_name_plural': 'users',
             },
+            bases=('auth.user', models.Model),
+            managers=[
+                ('objects', django.contrib.auth.models.UserManager()),
+            ],
         ),
         migrations.CreateModel(
             name='Student',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
-                ('username', models.CharField(max_length=50)),
-                ('password', models.CharField(max_length=50)),
+                ('user_ptr', models.OneToOneField(primary_key=True, serialize=False, auto_created=True, parent_link=True, to=settings.AUTH_USER_MODEL)),
                 ('number_id', models.IntegerField()),
-                ('email', models.EmailField(max_length=200)),
-                ('name', models.CharField(max_length=50)),
-                ('admin', models.BooleanField()),
             ],
             options={
                 'abstract': False,
+                'verbose_name': 'user',
+                'verbose_name_plural': 'users',
             },
+            bases=('auth.user', models.Model),
+            managers=[
+                ('objects', django.contrib.auth.models.UserManager()),
+            ],
         ),
         migrations.AddField(
             model_name='avaliation',
